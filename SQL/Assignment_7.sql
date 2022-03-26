9d2858118b5892ae4b05bc66423a98d6458a59f3
joma13331 <78799226+joma13331@users.noreply.github.com>
2022-03-26 21:47:51 +0530

    Assignment 7 Completed

---
+++ SQL/Assignment_7.sql
@@ -0,0 +1,42 @@
+# CREATING DATABASE
+create database assignments;
+
+# USE ASSIGNMENT DATABASE
+use assignments;
+
+# CREATING TABLE
+CREATE TABLE worker(
+worker_id INT PRIMARY KEY,
+first_name VARCHAR(20),
+last_name VARCHAR(20),
+department VARCHAR(20),
+salary INT,
+email VARCHAR(30),
+joining_date DATE
+);
+
+# INSERTING VALUES INTO THE worker TABLE
+INSERT INTO worker(worker_id, first_name, last_name, department, salary,email,joining_date)
+VALUES
+(1, "Jobin", "Mathew", "Electrical", 70000, "jobinmathewsp@gmail.com", "2018-01-04"),
+(2, "Vipul", "Kumar", "Mechanical", 35000, "satishkumar@gmail.com", "2012-06-24"),
+(3, "Satish", "Choudhary", "Workshop", 25000, "satishchoudhary@gmail.com", "2010-03-14"),
+(4, "Amit", "Jana", "IT", 100000, "amitjana@gmail.com", "2004-12-22"),
+(5, "Manik", "Aggarwal", "IT", 60000, "manikaggarwal@gmail.com", "2006-07-04"),
+(6, "Priya", "Pal", "Civil", 120000, "priyapal@gmail.com", "2020-06-20"),
+(7, "Biswanath", "Sharma", "Electrical", "170000", "biswanathsharma@gmail.com", "2016-09-24"),
+(8, "Mani", "Punia", "Chemical", 80000, "manipunia@gmail.com", "2006-01-20"),
+(9, "Pritam", "Patel", "Mechanical", 40000, "pritampatel@gmail.com", "2016-03-01"),
+(10, "Sayak", "Sharma", "Workshop", 20000, "sayaksharma@gmail.com", "2007-05-10");
+
+# Q13. Write an SQL query to show only odd rows from a table.
+Select * from
+(SELECT * , row_number() over() as row_num
+from worker) temp
+where mod(temp.row_num,2)=1;
+
+# Q14. Write an SQL query to clone a new table from another table.
+CREATE TABLE worker_two LIKE worker;
+INSERT INTO worker_two SELECT * FROM worker;
+
+SELECT * FROM worker_two;
